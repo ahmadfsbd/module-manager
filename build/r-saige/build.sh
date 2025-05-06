672b953c391c4f762c8c818bdbe64a9ad0c4ee68
Muhammad Ahmad <ma30@sanger.ac.uk>
2025-05-06 13:05:10 +0000

    create meta.yaml inplace of exports file

--- build/r-saige/build.sh
+++ build/r-saige/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# Module path, where .sif and exports will be created
-modulepath="../../modules/R-SAIGE"
+# Variables: These should be changed/updated for each module
 
-# List of commands to add to exports (each on a new line)
-# Example: exports=("R" "python")
+#### Variables Start ####
+
+# Module path, where .sif and meta.yaml will be created
+modulepath="../../modules/R-SAIGE"
+# List of commands to add to export. Example: exports=("R" "python")
 exports=("R")
+# Module Description
+description="This module exports the R command and includes the SAIGE package for R."
+# Optional: list of packages (can be left empty or populated manually)
+packages=("SAIGE")
+
+#### Variables End ####
 
 # Create module path (and overwrite existing .sif if it exists)
 mkdir -p "$modulepath"
@@ -14,12 +22,19 @@ rm -f "$modulepath/singularity.sif"
 # Build the Singularity image
 singularity build --fakeroot "$modulepath/singularity.sif" singularity.def
 
-# Path to the exports file
-exports_file="$modulepath/exports"
+# Generate meta.yaml
+meta_file="$modulepath/meta.yaml"
+{
+    echo "description: \"$description\""
+    echo "packages:"
+    for pkg in "${packages[@]}"; do
+        echo "  - $pkg"
+    done
+    echo "executables:"
+    for exe in "${exports[@]}"; do
+        echo "  - $exe"
+    done
+} > "$meta_file"
 
-# Overwrite the exports file with the new entries
-: > "$exports_file"
-for cmd in "${exports[@]}"; do
-	  echo "$cmd" >> "$exports_file"
-  done
+echo "Module created at $modulepath with meta.yaml."
 
